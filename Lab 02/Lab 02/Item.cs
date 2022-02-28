7d1280a0f9a00e5bdcc845c694bb27a686a6d4fb
Ryan-Atkins3 <Ryan_Atkins@insideranken.org>
2022-02-28 11:40:35 -0600

    Item class

--- Lab 02/Lab 02/Item.cs
+++ Lab 02/Lab 02/Item.cs
@@ -8,5 +8,54 @@ namespace Lab_02
 {
     public class Item
     {
+        private string _name;
+        private string _upc;
+        private double _storePrice;
+        private string _accessKey;
+        private double _costPerCase;
+        private int _units;
+        private string distributor;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Upc
+        {
+            get { return _upc; }
+            set { _upc = value; }
+        }
+
+        public double StorePrice
+        {
+            get { return _storePrice; }
+            set { _storePrice = value; }
+        }
+
+        public string AccessKey
+        {
+            get { return _accessKey; }
+            set { _accessKey = value; }
+        }
+
+        public double CostPerCase
+        {
+            get { return _costPerCase; }
+            set { _costPerCase = value; }
+        }
+
+        public int UnitCount
+        {
+            get { return _units; }
+            set { _units = value; }
+        }
+
+        public string Distributor
+        {
+            get { return distributor; }
+            set { distributor = value; }
+        }
     }
 }
